84fae6d50e93e83158c2447c4ea906714ccb53f6
renasrenas2 <renato.moreira.faria@gmail.com>
2026-08-03 13:42:02 -0300

    Modificando o programa dragonball(renato).py

--- dragonball(renato).py
+++ dragonball(renato).py
@@ -49,28 +49,62 @@ def main():
     racas = ["Saiyajin", "Androide", "Namekuseijin"]
 
     while True:
-        print("===Torneio Dragon Ball===")
+        print("\n===Torneio Dragon Ball===\n")
         print("1 - Cadastrar Lutador")
         print("2 - Listar Lutadores inscritos")
         print("3 - Atacar com um lutador")
         print("4 - Sair")
 
-        opcao = input("Escolha uma opção (1-4): ")
+        opcao = input("\nEscolha uma opção (1-4): ")
 
         if opcao == "1":
+            print()
             for i, raca in enumerate(racas):
-                print(f"{i} - {raca}")
+                print(f"{i+1} - {raca}")
+            print()
 
-            escolha = input("Escolha uma raça: ")
-            
-            nome = input("Nome: ")
-            poder = input("Poder: ")
-            veiculos.append(Onibus(placa, capacidade, consumo))
-            torneio.append()
+            escolha = int(input("Escolha uma raça (1-3): "))
 
+            if escolha == 1:
+                nome = input("Nome: ")
+                poder = input("Poder: ")
+                torneio.append(Saiyajin(nome, poder))
+                print("Lutador adicionado com sucesso!")
 
+            elif escolha == 2:
+                nome = input("Nome: ")
+                poder = input("Poder: ")
+                torneio.append(Androide(nome, poder))
+                print("Lutador adicionado com sucesso!")
 
+            elif escolha == 3:
+                nome = input("Nome: ")
+                poder = input("Poder: ")
+                torneio.append(Namekuseijin(nome, poder))
+                print("Lutador adicionado com sucesso!")
         
+        elif opcao == "2":
+            print("\n===Lista de lutadores inscritos===\n")
+            for l in torneio:
+                print(f"Nome: {nome} - Poder: {poder}")
+            print()
+        
+        elif opcao == "3":
+            for i, l in enumerate(torneio):
+                print(f"{i+1} - {l}")
+            print()
+            
+            lutador = int(input("Escolha um lutador: "))
+
+            lutador.usar_ataque()
+        
+        elif opcao == "4":
+            print("Encerrando torneio...")
+            break
+        
+        else:
+            print("Opção inválida! Digite novamente.")
+
 if __name__ == "__main__":
     main()
         
